3a46bf83236cc993c07fe4de409404711b49f139
Manuel <Ramirez>
2020-05-04 20:11:22 -0400

    rstart

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,21 @@
-import React from "react";
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Books from "./pages/Books";
-import Saved from "./pages/Saved";
-import Home from "./pages/Home";
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-
-function App() {
-
-  return (
-    <ThemeProvider theme={createMuiTheme({})}>
-      <Router>
-        <Navbar>
-        <Container>
-          <Switch>
-            <Route path="/booksearch" component={Books} />
-            <Route path="/saved" component={Saved} />
-            <Route path="/" component={Home} />
-            {/* <Books /> */}
-            <Saved />
-          </Switch>
-        </Container>
-        </Navbar>
-      </Router>
-    </ThemeProvider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/utils/GoogleAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default {
-
-    findBooks: function(search){
-       return axios.get("https://www.googleapis.com/books/v1/volumes?q="+search);
-    }
-}
